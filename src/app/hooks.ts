404ca8df632b87f5f7b6ea8438545ceaa7c92b06
Craig Stroman <cstroman@gmail.com>
2022-01-03 15:54:41 -0500

    Starting to add redux

---
+++ src/app/hooks.ts
@@ -0,0 +1,9 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { Dispatch } from 'react';
+import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
+import type { RootState, AppDispatch } from './store';
+
+// Use throughout your app instead of plain `useDispatch` and `useSelector`
+export const useAppDispatch = (): Dispatch<unknown> =>
+  useDispatch<AppDispatch>();
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
