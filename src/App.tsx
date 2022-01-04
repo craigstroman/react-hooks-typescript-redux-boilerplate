d1f4a591fe937940134bb7c80013e3574cfa77dd
Craig Stroman <cstroman@gmail.com>
2022-01-03 23:13:57 -0500

    Adding home component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Home from './containers/Home/Home';
 
 const App: React.FC = () => (
-  <div className="wrapper">
-    <h1>React 17 and TypeScript 4 App!🚀</h1>
-  </div>
+  <BrowserRouter>
+    <Switch>
+      <Route path="/" component={Home} />
+    </Switch>
+  </BrowserRouter>
 );
 
 export default App;
