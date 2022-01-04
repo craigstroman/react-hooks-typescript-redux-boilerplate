0e5d48b87b12e6d1e25cbfee11be1ae964a988aa
Craig Stroman <cstroman@gmail.com>
2022-01-03 23:14:27 -0500

    Adding route

--- src/containers/Home/Home.tsx
+++ src/containers/Home/Home.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
 import { Dashboard } from '../Dashboard/Dashboard';
+import { Posts } from '../Posts/Posts';
 
 const Home = () => {
   return (
     <Switch>
       <Route exact path="/" component={Dashboard} />
+      <Route exact path="/posts" component={Posts} />
     </Switch>
   );
 };
