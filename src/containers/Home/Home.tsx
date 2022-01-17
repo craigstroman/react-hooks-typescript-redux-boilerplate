3a58ea910f23eb1238890b1a4d779b17c7fa54e4
Craig Stroman <cstroman@gmail.com>
2022-01-17 16:06:55 -0500

    Adding users component

--- src/containers/Home/Home.tsx
+++ src/containers/Home/Home.tsx
@@ -3,12 +3,14 @@ import { Switch, Route } from 'react-router-dom';
 
 import { Dashboard } from '../Dashboard/Dashboard';
 import { Posts } from '../Posts/Posts';
+import { Users } from '../Users/Users';
 
 const Home = () => {
   return (
     <Switch>
       <Route exact path="/" component={Dashboard} />
       <Route exact path="/posts" component={Posts} />
+      <Route exact path="/users" component={Users} />
     </Switch>
   );
 };
