8ee5d55cbb2217cc3ec1705cb97558ceb3de0d46
Craig Stroman <cstroman@gmail.com>
2022-01-17 16:38:07 -0500

    Creating user items component

---
+++ src/components/UserItems/UserItems.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { IUsers } from '../../containers/Users/usersTypes';
+import './userItems.scss';
+
+interface UserItemsProps {
+  loading: boolean;
+  error: string;
+  users: IUsers[];
+}
+
+export const UserItems: React.FC<UserItemsProps> = ({
+  loading,
+  error,
+  users,
+}) => {
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>There was an error: {error}.</div>;
+  }
+
+  return (
+    <ul className="users">
+      {users.map((user) => (
+        <li key={`${user.id}-${Math.random() * 10}`}>
+          <Link to={`/user/${user.id}`} className="users__link">
+            {user.name}
+          </Link>
+        </li>
+      ))}
+    </ul>
+  );
+};
