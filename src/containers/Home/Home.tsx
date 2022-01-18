6fcc085bb20633d1448710236921ada1dfead002
Craig Stroman <cstroman@gmail.com>
2022-01-17 21:22:45 -0500

    Adding post item path

--- src/containers/Home/Home.tsx
+++ src/containers/Home/Home.tsx
@@ -4,12 +4,14 @@ import { Switch, Route } from 'react-router-dom';
 import { Dashboard } from '../Dashboard/Dashboard';
 import { Posts } from '../Posts/Posts';
 import { Users } from '../Users/Users';
+import { PostItem } from '../../components/PostItem/PostItem';
 
 const Home = () => {
   return (
     <Switch>
       <Route exact path="/" component={Dashboard} />
       <Route exact path="/posts" component={Posts} />
+      <Route exact path="/post/:id" component={PostItem} />
       <Route exact path="/users" component={Users} />
     </Switch>
   );
