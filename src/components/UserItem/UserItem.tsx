6f3df1cfa027cfc1fe4277cccc639d3f9c7f68bf
Craig Stroman <cstroman@gmail.com>
2022-01-22 15:56:39 -0500

    Adding more user properties to component

--- src/components/UserItem/UserItem.tsx
+++ src/components/UserItem/UserItem.tsx
@@ -13,11 +13,10 @@ export const UserItem: React.FC = () => {
   const userState: IUser = useLocation();
   const { state } = userState;
   const { user } = state;
-  const { email } = user;
-  const { name } = user;
-  const { username } = user;
-  const { website } = user;
-  const { phone } = user;
+  const { address, company, email, name, username, website, phone } = user;
+  const { city, street, suite, zipcode } = address;
+
+  console.log('user: ', user);
 
   return (
     <div className="user">
@@ -46,6 +45,34 @@ export const UserItem: React.FC = () => {
             {website}
           </a>
         </div>
+      </div>
+      <div className="user__info">
+        <div className="user__info--label">Street:</div>
+        <div className="user__info--item">{street}</div>
+      </div>
+      <div className="user__info">
+        <div className="user__info--label">Suite:</div>
+        <div className="user__info--item">{suite}</div>
+      </div>
+      <div className="user__info">
+        <div className="user__info--label">City:</div>
+        <div className="user__info--item">{city}</div>
+      </div>
+      <div className="user__info">
+        <div className="user__info--label">Zipcode:</div>
+        <div className="user__info--item">{zipcode}</div>
+      </div>
+      <div className="user__info">
+        <div className="user__info--label">Company:</div>
+        <div className="user__info--item">{company.name}</div>
+      </div>
+      <div className="user__info">
+        <div className="user__info--label">Catch Phrase:</div>
+        <div className="user__info--item">{company.catchPhrase}</div>
+      </div>
+      <div className="user__info">
+        <div className="user__info--label">BS:</div>
+        <div className="user__info--item">{company.bs}</div>
       </div>
     </div>
   );
